ad9f51f66d87c965b73cef4a0ce42415ea826c70
Larry Zheng <zhiyuan.zheng@outlook.com>
2017-07-18 00:47:23 +1200

    添加时间线板块，在加载js代码前读取大图

---
+++ AR_Project1/wwwroot/js/disable-scrolling.js
@@ -0,0 +1,50 @@
+﻿// set id enable and disable to elements to controll
+document.getElementById("enable").onclick = function () {
+    enableScroll();
+    document.getElementById("status").innerHTML = "enabled";
+    document.getElementById("status").className = "enabled";
+};
+
+document.getElementById("disable").onclick = function () {
+    disableScroll();
+    document.getElementById("status").innerHTML = "disabled";
+    document.getElementById("status").className = "disabled";
+};
+
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 32: 1, 33: 1, 34: 1, 35: 1, 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+    if (window.addEventListener) // older FF
+        window.addEventListener('DOMMouseScroll', preventDefault, false);
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove = preventDefault; // mobile
+    document.onkeydown = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
